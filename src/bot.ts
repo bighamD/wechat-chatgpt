7ceffba4ec0b1f60dd9c36b361acb4c97c1c7418
pengdahan <805359052@qq.com>
2023-08-31 15:45:19 +0800

    feat: 调试不生效问题

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,15 @@
-import { config } from "./config.js";
-import {ContactImpl, ContactInterface, RoomImpl, RoomInterface} from "wechaty/impls";
-import { Message } from "wechaty";
-import {FileBox} from "file-box";
-import {chatgpt, dalle, whisper} from "./openai.js";
-import DBUtils from "./data.js";
-import { regexpEncode } from "./utils.js";
+import { config } from './config.js'
+import {
+  ContactImpl,
+  ContactInterface,
+  RoomImpl,
+  RoomInterface
+} from 'wechaty/impls'
+import { Message } from 'wechaty'
+import { FileBox } from 'file-box'
+import { chatgpt, dalle, whisper } from './openai.js'
+import DBUtils from './data.js'
+import { regexpEncode } from './utils.js'
 enum MessageType {
   Unknown = 0,
   Attachment = 1, // Attach(6),
@@ -22,71 +27,76 @@ enum MessageType {
   Recalled = 13, // Recalled(10002)
   Url = 14, // Url(5)
   Video = 15, // Video(4), Video(43)
-  Post = 16, // Moment, Channel, Tweet, etc
+  Post = 16 // Moment, Channel, Tweet, etc
 }
-const SINGLE_MESSAGE_MAX_SIZE = 500;
-type Speaker = RoomImpl | ContactImpl;
-interface ICommand{
-  name:string;
-  description:string;
-  exec: (talker:Speaker, text:string) => Promise<void>;
+const SINGLE_MESSAGE_MAX_SIZE = 500
+type Speaker = RoomImpl | ContactImpl
+interface ICommand {
+  name: string
+  description: string
+  exec: (talker: Speaker, text: string) => Promise<void>
 }
 export class ChatGPTBot {
-  chatPrivateTriggerKeyword = config.chatPrivateTriggerKeyword;
-  chatTriggerRule = config.chatTriggerRule? new RegExp(config.chatTriggerRule): undefined;
-  disableGroupMessage = config.disableGroupMessage || false;
-  botName: string = "";
-  ready = false;
-  setBotName(botName: string) {
-    this.botName = botName;
+  chatPrivateTriggerKeyword = config.chatPrivateTriggerKeyword
+  chatTriggerRule = config.chatTriggerRule
+    ? new RegExp(config.chatTriggerRule)
+    : undefined
+  disableGroupMessage = config.disableGroupMessage || false
+  botName: string = ''
+  ready = false
+  setBotName (botName: string) {
+    this.botName = botName
   }
-  get chatGroupTriggerRegEx(): RegExp {
-    return new RegExp(`^@${regexpEncode(this.botName)}\\s`);
+  get chatGroupTriggerRegEx (): RegExp {
+    return new RegExp(`^@${regexpEncode(this.botName)}\\s`)
   }
-  get chatPrivateTriggerRule(): RegExp | undefined {
-    const { chatPrivateTriggerKeyword, chatTriggerRule } = this;
+  get chatPrivateTriggerRule (): RegExp | undefined {
+    const { chatPrivateTriggerKeyword, chatTriggerRule } = this
     let regEx = chatTriggerRule
     if (!regEx && chatPrivateTriggerKeyword) {
       regEx = new RegExp(regexpEncode(chatPrivateTriggerKeyword))
     }
     return regEx
   }
-  private readonly commands:ICommand[] = [
+  private readonly commands: ICommand[] = [
     {
-      name: "help",
-      description: "显示帮助信息",
-      exec: async (talker) => {
-        await this.trySay(talker,"========\n" +
-          "/cmd help\n" +
-          "# 显示帮助信息\n" +
-          "/cmd prompt <PROMPT>\n" +
-          "# 设置当前会话的 prompt \n" +
-          "/img <PROMPT>\n" +
-          "# 根据 prompt 生成图片\n" +
-          "/cmd clear\n" +
-          "# 清除自上次启动以来的所有会话\n" +
-          "========");
+      name: 'help',
+      description: '显示帮助信息',
+      exec: async talker => {
+        await this.trySay(
+          talker,
+          '========\n' +
+            '/cmd help\n' +
+            '# 显示帮助信息\n' +
+            '/cmd prompt <PROMPT>\n' +
+            '# 设置当前会话的 prompt \n' +
+            '/img <PROMPT>\n' +
+            '# 根据 prompt 生成图片\n' +
+            '/cmd clear\n' +
+            '# 清除自上次启动以来的所有会话\n' +
+            '========'
+        )
       }
     },
     {
-      name: "prompt",
-      description: "设置当前会话的prompt",
+      name: 'prompt',
+      description: '设置当前会话的prompt',
       exec: async (talker, prompt) => {
         if (talker instanceof RoomImpl) {
-          DBUtils.setPrompt(await talker.topic(), prompt);
-        }else {
-          DBUtils.setPrompt(talker.name(), prompt);
+          DBUtils.setPrompt(await talker.topic(), prompt)
+        } else {
+          DBUtils.setPrompt(talker.name(), prompt)
         }
       }
     },
     {
-      name: "clear",
-      description: "清除自上次启动以来的所有会话",
-      exec: async (talker) => {
+      name: 'clear',
+      description: '清除自上次启动以来的所有会话',
+      exec: async talker => {
         if (talker instanceof RoomImpl) {
-          DBUtils.clearHistory(await talker.topic());
-        }else{
-          DBUtils.clearHistory(talker.name());
+          DBUtils.clearHistory(await talker.topic())
+        } else {
+          DBUtils.clearHistory(talker.name())
         }
       }
     }
@@ -101,97 +111,97 @@ export class ChatGPTBot {
    * @param contact
    * @param rawText
    */
-  async command(contact: any, rawText: string): Promise<void> {
-    const [commandName, ...args] = rawText.split(/\s+/);
-    const command = this.commands.find(
-      (command) => command.name === commandName
-    );
+  async command (contact: any, rawText: string): Promise<void> {
+    const [commandName, ...args] = rawText.split(/\s+/)
+    const command = this.commands.find(command => command.name === commandName)
     if (command) {
-      await command.exec(contact, args.join(" "));
+      await command.exec(contact, args.join(' '))
     }
   }
   // remove more times conversation and mention
-  cleanMessage(rawText: string, privateChat: boolean = false): string {
-    let text = rawText;
-    const item = rawText.split("- - - - - - - - - - - - - - -");
+  cleanMessage (rawText: string, privateChat: boolean = false): string {
+    let text = rawText
+    const item = rawText.split('- - - - - - - - - - - - - - -')
     if (item.length > 1) {
-      text = item[item.length - 1];
+      text = item[item.length - 1]
     }
 
-    const { chatTriggerRule, chatPrivateTriggerRule } = this;
+    const { chatTriggerRule, chatPrivateTriggerRule } = this
 
     if (privateChat && chatPrivateTriggerRule) {
-      text = text.replace(chatPrivateTriggerRule, "")
+      text = text.replace(chatPrivateTriggerRule, '')
     } else if (!privateChat) {
-      text = text.replace(this.chatGroupTriggerRegEx, "")
-      text = chatTriggerRule? text.replace(chatTriggerRule, ""): text
+      text = text.replace(this.chatGroupTriggerRegEx, '')
+      text = chatTriggerRule ? text.replace(chatTriggerRule, '') : text
     }
     // remove more text via - - - - - - - - - - - - - - -
     return text
   }
-  async getGPTMessage(talkerName: string,text: string): Promise<string> {
-    let gptMessage = await chatgpt(talkerName,text);
-    if (gptMessage !=="") {
-      DBUtils.addAssistantMessage(talkerName,gptMessage);
-      return gptMessage;
+  async getGPTMessage (talkerName: string, text: string): Promise<string> {
+    let gptMessage = await chatgpt(talkerName, text)
+    if (gptMessage !== '') {
+      DBUtils.addAssistantMessage(talkerName, gptMessage)
+      return gptMessage
     }
-    return "Sorry, please try again later. 😔";
+    return 'Sorry, please try again later. 😔'
   }
   // Check if the message returned by chatgpt contains masked words]
-  checkChatGPTBlockWords(message: string): boolean {
+  checkChatGPTBlockWords (message: string): boolean {
     if (config.chatgptBlockWords.length == 0) {
-      return false;
+      return false
     }
-    return config.chatgptBlockWords.some((word) => message.includes(word));
+    return config.chatgptBlockWords.some(word => message.includes(word))
   }
   // The message is segmented according to its size
-  async trySay(
+  async trySay (
     talker: RoomInterface | ContactInterface,
     mesasge: string
   ): Promise<void> {
-    const messages: Array<string> = [];
+    const messages: Array<string> = []
     if (this.checkChatGPTBlockWords(mesasge)) {
-      console.log(`🚫 Blocked ChatGPT: ${mesasge}`);
-      return;
+      console.log(`🚫 Blocked ChatGPT: ${mesasge}`)
+      return
     }
-    let message = mesasge;
+    let message = mesasge
     while (message.length > SINGLE_MESSAGE_MAX_SIZE) {
-      messages.push(message.slice(0, SINGLE_MESSAGE_MAX_SIZE));
-      message = message.slice(SINGLE_MESSAGE_MAX_SIZE);
+      messages.push(message.slice(0, SINGLE_MESSAGE_MAX_SIZE))
+      message = message.slice(SINGLE_MESSAGE_MAX_SIZE)
     }
-    messages.push(message);
+    messages.push(message)
     for (const msg of messages) {
-      await talker.say(msg);
+      await talker.say(msg)
     }
   }
   // Check whether the ChatGPT processing can be triggered
-  triggerGPTMessage(text: string, privateChat: boolean = false): boolean {
-    const { chatTriggerRule } = this;
-    let triggered = false;
+  triggerGPTMessage (text: string, privateChat: boolean = false): boolean {
+    const { chatTriggerRule } = this
+    let triggered = false
     if (privateChat) {
       const regEx = this.chatPrivateTriggerRule
-      triggered = regEx? regEx.test(text): true;
+      triggered = regEx ? regEx.test(text) : true
     } else {
-      triggered = this.chatGroupTriggerRegEx.test(text);
+      triggered = this.chatGroupTriggerRegEx.test(text)
       // group message support `chatTriggerRule`
       if (triggered && chatTriggerRule) {
-        triggered = chatTriggerRule.test(text.replace(this.chatGroupTriggerRegEx, ""))
+        triggered = chatTriggerRule.test(
+          text.replace(this.chatGroupTriggerRegEx, '')
+        )
       }
     }
     if (triggered) {
-      console.log(`🎯 Triggered ChatGPT: ${text}`);
+      console.log(`🎯 Triggered ChatGPT: ${text}`)
     }
-    return triggered;
+    return triggered
   }
   // Check whether the message contains the blocked words. if so, the message will be ignored. if so, return true
-  checkBlockWords(message: string): boolean {
+  checkBlockWords (message: string): boolean {
     if (config.blockWords.length == 0) {
-      return false;
+      return false
     }
-    return config.blockWords.some((word) => message.includes(word));
+    return config.blockWords.some(word => message.includes(word))
   }
   // Filter out the message that does not need to be processed
-  isNonsense(
+  isNonsense (
     talker: ContactInterface,
     messageType: MessageType,
     text: string
@@ -200,101 +210,103 @@ export class ChatGPTBot {
       talker.self() ||
       // TODO: add doc support
       !(messageType == MessageType.Text || messageType == MessageType.Audio) ||
-      talker.name() === "微信团队" ||
+      talker.name() === '微信团队' ||
       // 语音(视频)消息
-      text.includes("收到一条视频/语音聊天消息，请在手机上查看") ||
+      text.includes('收到一条视频/语音聊天消息，请在手机上查看') ||
       // 红包消息
-      text.includes("收到红包，请在手机上查看") ||
+      text.includes('收到红包，请在手机上查看') ||
       // Transfer message
-      text.includes("收到转账，请在手机上查看") ||
+      text.includes('收到转账，请在手机上查看') ||
       // 位置消息
-      text.includes("/cgi-bin/mmwebwx-bin/webwxgetpubliclinkimg") ||
+      text.includes('/cgi-bin/mmwebwx-bin/webwxgetpubliclinkimg') ||
       // 聊天屏蔽词
       this.checkBlockWords(text)
-    );
+    )
   }
 
-  async onPrivateMessage(talker: ContactInterface, text: string) {
-    const gptMessage = await this.getGPTMessage(talker.name(),text);
-    await this.trySay(talker, gptMessage);
+  async onPrivateMessage (talker: ContactInterface, text: string) {
+    const gptMessage = await this.getGPTMessage(talker.name(), text)
+    await this.trySay(talker, gptMessage)
   }
 
-  async onGroupMessage(
+  async onGroupMessage (
     talker: ContactInterface,
     text: string,
     room: RoomInterface
   ) {
-    const gptMessage = await this.getGPTMessage(await room.topic(),text);
-    const result = `@${talker.name()} ${text}\n\n------\n ${gptMessage}`;
-    await this.trySay(room, result);
+    const gptMessage = await this.getGPTMessage(await room.topic(), text)
+    const result = `@${talker.name()} ${text}\n\n------\n ${gptMessage}`
+    await this.trySay(room, result)
   }
-  async onMessage(message: Message) {
-    const talker = message.talker();
-    const rawText = message.text();
-    const room = message.room();
-    const messageType = message.type();
-    const privateChat = !room;
+  async onMessage (message: Message) {
+    const talker = message.talker()
+    const rawText = message.text()
+    const room = message.room()
+    const messageType = message.type()
+    const privateChat = !room
     if (privateChat) {
       console.log(`🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`)
     } else {
       const topic = await room.topic()
-      console.log(`🚪 Room: ${topic} 🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`)
+      console.log(
+        `🚪 Room: ${topic} 🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`
+      )
     }
-    if (this.isNonsense(talker, messageType, rawText)) {
-      return;
-    }
-    if (messageType == MessageType.Audio){
+    // if (this.isNonsense(talker, messageType, rawText)) {
+    //   return;
+    // }
+    if (messageType == MessageType.Audio) {
       // 保存语音文件
-      const fileBox = await message.toFileBox();
-      let fileName = "./public/" + fileBox.name;
-      await fileBox.toFile(fileName, true).catch((e) => {
-        console.log("保存语音失败",e);
-        return;
-      });
-      // Whisper
-      whisper("",fileName).then((text) => {
-        message.say(text);
+      const fileBox = await message.toFileBox()
+      let fileName = './public/' + fileBox.name
+      await fileBox.toFile(fileName, true).catch(e => {
+        console.log('保存语音失败', e)
+        return
       })
-      return;
+      // Whisper
+      whisper('', fileName).then(text => {
+        message.say(text)
+      })
+      return
     }
-    if (rawText.startsWith("/cmd ")){
+    if (rawText.startsWith('/cmd ')) {
       console.log(`🤖 Command: ${rawText}`)
       const cmdContent = rawText.slice(5) // 「/cmd 」一共5个字符(注意空格)
       if (privateChat) {
-        await this.command(talker, cmdContent);
-      }else{
-        await this.command(room, cmdContent);
+        await this.command(talker, cmdContent)
+      } else {
+        await this.command(room, cmdContent)
       }
-      return;
+      return
     }
     // 使用DallE生成图片
-    if (rawText.startsWith("/img")){
+    if (rawText.startsWith('/img')) {
       console.log(`🤖 Image: ${rawText}`)
       const imgContent = rawText.slice(4)
       if (privateChat) {
-        let url = await dalle(talker.name(), imgContent) as string;
+        let url = (await dalle(talker.name(), imgContent)) as string
         const fileBox = FileBox.fromUrl(url)
         message.say(fileBox)
-      }else{
-        let url = await dalle(await room.topic(), imgContent) as string;
+      } else {
+        let url = (await dalle(await room.topic(), imgContent)) as string
         const fileBox = FileBox.fromUrl(url)
         message.say(fileBox)
       }
-      return;
+      return
     }
     if (this.triggerGPTMessage(rawText, privateChat)) {
-      const text = this.cleanMessage(rawText, privateChat);
+      const text = this.cleanMessage(rawText, privateChat)
       if (privateChat) {
-        return await this.onPrivateMessage(talker, text);
-      } else{
-        if (!this.disableGroupMessage){
-          return await this.onGroupMessage(talker, text, room);
+        return await this.onPrivateMessage(talker, text)
+      } else {
+        if (!this.disableGroupMessage) {
+          return await this.onGroupMessage(talker, text, room)
         } else {
-          return;
+          return
         }
       }
     } else {
-      return;
+      return
     }
   }
 }
--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,12 @@ dotenv.config()
 import { IConfig } from './interface'
 
 export const config: IConfig = {
-  api: process.env.API || 'https://api.openai.com',
-  openai_api_key:
-    process.env.OPENAI_API_KEY ||
-    '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',
+  api: process.env.API,
+  openai_api_key: process.env.OPENAI_API_KEY || '123456789',
   model: process.env.MODEL || 'gpt-3.5-turbo',
-  chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || 'AI',
+  chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || '',
   chatTriggerRule: process.env.CHAT_TRIGGER_RULE || '',
-  disableGroupMessage: false,
+  disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === 'true',
   temperature: process.env.TEMPERATURE
     ? parseFloat(process.env.TEMPERATURE)
     : 0.6,
